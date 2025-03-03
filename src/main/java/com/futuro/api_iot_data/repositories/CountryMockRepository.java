0c0ba8a6f15d0f5d74a55bef0243acd8b0c4e57e
Stanley Bien Aime <bienaimestanley7@gmail.com>
2025-03-03 18:38:11 -0300

    feat: Creación de test unitarios para locationservice

---
+++ src/main/java/com/futuro/api_iot_data/repositories/CountryMockRepository.java
@@ -0,0 +1,9 @@
+package com.futuro.api_iot_data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.futuro.api_iot_data.models.CountryMock;
+
+public interface CountryMockRepository extends JpaRepository<CountryMock,Long> {
+
+}
