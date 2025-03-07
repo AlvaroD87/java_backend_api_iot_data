0ff251e4284483b230ef169cd3c0a72ba830ad37
Stanley Bien Aime <bienaimestanley7@gmail.com>
2025-03-07 17:38:57 -0300

    Refractor: Actualización de test unitarios de locaciones
    - Ahora se utilizará mockito para las pruebas unitarias de locaciones
      para evitar la dependencia de Docker al usar testcontainers

--- src/test/java/com/futuro/api_iot_data/LocationServiceTest.java
+++ src/test/java/com/futuro/api_iot_data/LocationServiceTest.java
@@ -1,25 +1,21 @@
 package com.futuro.api_iot_data;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.futuro.api_iot_data.dtos.CityMockDTO;
 import com.futuro.api_iot_data.dtos.CompanyMockDTO;
@@ -36,177 +32,150 @@ import com.futuro.api_iot_data.repositories.CountryMockRepository;
 import com.futuro.api_iot_data.repositories.LocationRepository;
 import com.futuro.api_iot_data.services.LocationServiceImp;
 
-import jakarta.transaction.Transactional;
-
-@SpringBootTest
-@Testcontainers
-//@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
 public class LocationServiceTest {
-	
-	@Container
-    public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:13")
-            .withDatabaseName("testdb")
-            .withUsername("testuser")
-            .withPassword("testpass");
 
-    @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
-        registry.add("spring.jpa.hibernate.ddl-auto",() -> "create-drop");
-    }
-	@Autowired
-	AdminMockRepository adminRepository;
-	
-	@Autowired
-	CountryMockRepository countryRepository;
-	
-
-    @Autowired
-    private LocationServiceImp locationService;
-
-    @Autowired
+    @Mock
     private LocationRepository locationRepository;
 
-    @Autowired
+    @Mock
     private CompanyMockRepository companyRepository;
 
-    @Autowired
+    @Mock
     private CityMockRepository cityRepository;
-    
+
+    @Mock
+    private CountryMockRepository countryRepository;
+
+    @Mock
+    private AdminMockRepository adminRepository;
+
+    @InjectMocks
+    private LocationServiceImp locationService;
+
     private AdminMock admin;
     private CompanyMock company;
     private CountryMock country;
     private CityMock city;
     private LocationDTO locationDTO;
+    private Location location;
 
     @BeforeEach
     public void setUp() {
-    	
-    	
-        locationRepository.deleteAll();
-        companyRepository.deleteAll();
-        cityRepository.deleteAll();
-        countryRepository.deleteAll();
-        adminRepository.deleteAll();
-        
-       admin = AdminMock.builder()
-    			.username("admin")
-    			.password("1234")
-    			.isActive(true)
-    			.createdDate(new Timestamp(System.currentTimeMillis()))
-    			.updateDate(new Timestamp(System.currentTimeMillis()))
-    			.build();
-    	adminRepository.save(admin);
-    	
+        admin = AdminMock.builder()
+                .adminId(1L)
+                .username("admin")
+                .password("1234")
+                .isActive(true)
+                .createdDate(new Timestamp(System.currentTimeMillis()))
+                .updateDate(new Timestamp(System.currentTimeMillis()))
+                .build();
+
         company = new CompanyMock();
+        company.setCompanyId(1L);
         company.setCompanyName("Compañía de prueba");
         company.setCompanyApiKey("4324234234");
         company.setIsActive(true);
         company.setAdmin(admin);
         company.setCreatedDate(new Timestamp(System.currentTimeMillis()));
         company.setUpdateDate(new Timestamp(System.currentTimeMillis()));
-        company = companyRepository.save(company);
-        
-        
-        country = CountryMock.builder()
-        		.name("Chile")
-        		.isActive(true)
-        		.createdDate(new Timestamp(System.currentTimeMillis()))
-    			.updateDate(new Timestamp(System.currentTimeMillis()))
-        		.build();
-        
-        countryRepository.save(country);
-        
-        
 
-        city =  CityMock.builder()
-        		.name("Ciud de prueba")
-        		.isActive(true)
+        country = CountryMock.builder()
+                .countryId(1L)
+                .name("Chile")
+                .isActive(true)
                 .createdDate(new Timestamp(System.currentTimeMillis()))
-    			.updateDate(new Timestamp(System.currentTimeMillis()))
-    			.country(country)
-        		.build();
-        
-        city = cityRepository.save(city);
-        
+                .updateDate(new Timestamp(System.currentTimeMillis()))
+                .build();
+
+        city = CityMock.builder()
+                .cityId(1L)
+                .name("Ciudad de prueba")
+                .isActive(true)
+                .createdDate(new Timestamp(System.currentTimeMillis()))
+                .updateDate(new Timestamp(System.currentTimeMillis()))
+                .country(country)
+                .build();
+
         locationDTO = LocationDTO.builder()
+                .locationId(1L)
                 .locationName("Locación inicial")
                 .locationMeta(Map.of("clave uno", "valor 1"))
                 .companyDTO(CompanyMockDTO.builder().companyId(company.getCompanyId()).build())
                 .cityDTO(CityMockDTO.builder().cityId(city.getCityId()).build())
                 .isActive(true)
                 .createdDate(new Timestamp(System.currentTimeMillis()))
-    			.updateDate(new Timestamp(System.currentTimeMillis()))
+                .updateDate(new Timestamp(System.currentTimeMillis()))
                 .build();
 
-        locationDTO = locationService.create(locationDTO);
-        
-        
-        
+        location = Location.builder()
+                .locationId(1L)
+                .locationName("Locación inicial")
+                .locationMeta(Map.of("clave uno", "valor 1"))
+                .company(company)
+                .city(city)
+                .isActive(true)
+                .createdDate(new Timestamp(System.currentTimeMillis()))
+                .updateDate(new Timestamp(System.currentTimeMillis()))
+                .build();
     }
 
     @Test
     public void testCreateLocationSuccess() {
-    	
+        when(companyRepository.findById(any(Long.class))).thenReturn(Optional.of(company));
+        when(cityRepository.findById(any(Long.class))).thenReturn(Optional.of(city));
+        when(locationRepository.save(any(Location.class))).thenReturn(location);
 
-        LocationDTO newLocationDTO = LocationDTO.builder()
-                .locationName("Locación de prueba")
-                .locationMeta(Map.of("clave uno", "valor 1"))
-                .companyDTO(CompanyMockDTO.builder().companyId(company.getCompanyId()).build())
-                .cityDTO(CityMockDTO.builder().cityId(city.getCityId()).build())
-                .isActive(true)
-                .createdDate(new Timestamp(System.currentTimeMillis()))
-    			.updateDate(new Timestamp(System.currentTimeMillis()))
-                .build();
-
-        LocationDTO result = locationService.create(newLocationDTO);
+        LocationDTO result = locationService.create(locationDTO);
 
         assertNotNull(result.getLocationId());
-        assertEquals("Locación de prueba", result.getLocationName());
+        assertEquals("Locación inicial", result.getLocationName());
     }
 
     @Test
     public void testFindAllLocations() {
-        
+        when(locationRepository.findAll()).thenReturn(Collections.singletonList(location));
+
         List<LocationDTO> result = locationService.findAll();
 
         assertFalse(result.isEmpty());
         assertEquals(1, result.size());
         assertEquals("Locación inicial", result.get(0).getLocationName());
     }
-    
+
     @Test
-    public void testFindLocationByIdSuccess(){
-    	Long locationId = locationDTO.getLocationId();
-    	
-    	LocationDTO foundLocation = locationService.findById(locationId);
-    	
-    	assertNotNull(foundLocation.getLocationId());
-    	assertEquals(locationDTO.getLocationName(), foundLocation.getLocationName());
-    	
+    public void testFindLocationByIdSuccess() {
+        when(locationRepository.findById(any(Long.class))).thenReturn(Optional.of(location));
+
+        LocationDTO foundLocation = locationService.findById(1L);
+
+        assertNotNull(foundLocation.getLocationId());
+        assertEquals("Locación inicial", foundLocation.getLocationName());
     }
-    
+
     @Test
     public void testUpdateLocationSuccess() {
-    	Long locationId = locationDTO.getLocationId();
-    	String nombreActualizado = "Nombre de locación actualizado";
-    	locationDTO.setLocationName(nombreActualizado);
-    	locationService.update(locationId, locationDTO);
-    	
-    	LocationDTO updatedLocation = locationService.findById(locationId);
-    	assertEquals(nombreActualizado, updatedLocation.getLocationName());
-    	
+        when(locationRepository.findById(any(Long.class))).thenReturn(Optional.of(location));
+        when(locationRepository.save(any(Location.class))).thenReturn(location);
+        when(companyRepository.findById(any(Long.class))).thenReturn(Optional.of(company));
+        when(cityRepository.findById(any(Long.class))).thenReturn(Optional.of(city));
+
+        String nombreActualizado = "Nombre de locación actualizado";
+        locationDTO.setLocationName(nombreActualizado);
+        locationService.update(1L, locationDTO);
+
+        LocationDTO updatedLocation = locationService.findById(1L);
+        assertEquals(nombreActualizado, updatedLocation.getLocationName());
     }
-    
+
     @Test
-    public void testDeleteLocationByIsSuccess() {
-    	Long locationId = locationDTO.getLocationId();
-    	locationService.deleteById(locationId);
-    	
-    	assertNull(locationService.findById(locationId).getLocationId());
+    public void testDeleteLocationByIdSuccess() {
+        when(locationRepository.findById(any(Long.class))).thenReturn(Optional.of(location));
+        //doNothing().when(locationRepository).deleteById(any(Long.class));
+        when(locationRepository.findById(any(Long.class))).thenReturn(Optional.empty());
+    
+        locationService.deleteById(1L);
+    
+        assertNull(locationService.findById(1L).getLocationId());
     }
-    
-    
 }
