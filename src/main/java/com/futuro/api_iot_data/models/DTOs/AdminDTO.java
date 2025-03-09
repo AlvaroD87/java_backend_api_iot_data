ecc24a48c8f570d58abfffc50d64faff139bd956
Alvaro David Medel Espinoza <davidal.mes.87@gmail.com>
2025-03-09 18:30:05 -0300

    flujo de City crear y listar

--- src/main/java/com/futuro/api_iot_data/models/DTOs/AdminDTO.java
+++ src/main/java/com/futuro/api_iot_data/models/DTOs/AdminDTO.java
@@ -1,5 +1,7 @@
 package com.futuro.api_iot_data.models.DTOs;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ import lombok.ToString;
 public class AdminDTO implements ITemplateDTO{
 	
 	private String username;
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String password;
 
 }
